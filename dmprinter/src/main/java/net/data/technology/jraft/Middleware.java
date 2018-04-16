b3f58414518e9712e84c80dad44af23a91dfc51b
zoupeng <zoupeng@hotpu.cn>
2018-04-16 18:36:57 +0800

    20180416

--- dmprinter/src/main/java/net/data/technology/jraft/Middleware.java
+++ dmprinter/src/main/java/net/data/technology/jraft/Middleware.java
@@ -14,12 +14,30 @@ import java.util.concurrent.CompletableFuture;
  * 5、客户端连接数据转发
  * 6、节点之间实例服务切换
  * 监管实例建立(数据源 数据转发)、监管实例迁移(主动或故障)
+ * 解决思路，可考虑：
+ * 1、增加字段configType 配置类型分为：0 全局(hcm)、1应用配置(hcs leader分配 具体实例分配等 可能也为全配置，
+ * 只不过增加实例 应用节点id，以便leader切换时也能掌控切换)
+ * 即：一份全局配置(0)由HCM下发，供 leader(先选出再应用配置) 识别 全配置 并进行后续(实例等)分配，
+ * 包括重启时.
+ * ★考虑从slave对Master也有心跳(以便检测slave是否离线)
+ * 
+ * 
+ * 各集群节点完整配置包含(最终保存即一封完整的，leader连不上hcm时 即可读该份配置,进行操作)：
+ * ----HCM下发全局各节点配置(运行参数等等，整个集群需监控的实例相关等等) ---简写 配置Q
+ * ----缀加各节点实例分配配置  ---简写 配置 Z
+ * 
+ * 正常步骤：
+ * 、HCS启动后与HCM通讯 拿全局 配置Q
+ * 1、优先集群选出Leader 与 HCM进行通讯，等待HCM下发该组相关配置(运行参数、实例、集群节点等)
+ * 2、leader收到后 先同步一遍 commit之后 各节点保存，再由Leader计算实例分配
+ * 
+ * 重启步骤：
  * </pre>
  */
 public class Middleware implements StateMachine {
 
     /**
-     * 可以作为中间件相关启动初始化方法
+     * 可以作为中间件相关启动初始化方法()
      */
     @Override
     public void start(RaftMessageSender raftMessageSender) {
@@ -27,7 +45,11 @@ public class Middleware implements StateMachine {
 
     }
 
-    /* (non-Javadoc)
+    /**
+     * 针对提交的 日志 进行应用(考虑)
+     * 
+     * @param logIndex
+     * @param data
      * @see net.data.technology.jraft.StateMachine#commit(long, byte[])
      */
     @Override
