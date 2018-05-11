d8bf1d166e48496798bc6f8c5d3d986e33a4d42c
zoupeng <zoupeng@hotpu.cn>
2018-05-11 22:01:30 +0800

    20180511 JSON梳理

--- dmprinter/src/main/java/config.js
+++ dmprinter/src/main/java/config.js
@@ -1,5 +1,5 @@
 // http://www.sojson.com/simple_json.html
-//---------------------------完整集群编组信息-------------------------------------------------
+//---------------------------DEMO 完整集群编组信息-------------------------------------------------
 {
 	"taskId" : "uuid" , // 任务唯一Id, RS响应必须带该字段值
 	// --------私有配置 部分(后期细化)-----------------------
@@ -171,10 +171,110 @@ logger.warn(String.format(
 
 
 
-//--------------------------------------------------------测试用 JSON压缩版-------------------------------------------------------
+//--------------------------------------------------------DEMO 测试用 JSON压缩版-------------------------------------------------------
 下发配置  2   {"taskId":"uuid","systemConfig":{"heartbeatToM":2000,"usingAIO":1},"hcsGroup":[{"hcsId":"1","ip":"192.168.200.215","port":9001,"status":0,"sshPort":22,"isUsedPrvkey":false,"userName":"root","prvkeyFileContent":"AwABAgM=","password":"123456"},{"hcsId":"2","ip":"192.168.200.215","port":9002,"sshPort":22,"status":0,"isUsedPrvkey":false,"userName":"root","prvkeyFileContent":"AwABAgM=","password":"123456"},{"hcsId":"3","ip":"192.168.200.215","port":9003,"status":0,"sshPort":22,"isUsedPrvkey":false,"userName":"root","prvkeyFileContent":"AwABAgM=","password":"123456"}],"allocationRule":{"rule":0}}
 
 增加实例  21  {"taskId":"taskId21","rdsInstances":[{"rdsId":"HotDB_RDS_MM01_1","vip":"192.168.200.214","port":"3323","modeltype":2,"maxConnections":0,"maxUserConnections":0,"startAt":"2017-06-30 00:00:01","endAt":"2018-11-30 23:59:59","strategyForRWSplit":2,"weightForSlaveRWSplit":100,"status":1,"mysqlInfos":[{"id":1,"version":"5.6.1","ip":"127.0.0.1","port":3306,"manageUser":"root","managePassword":"64DF12CEBA7ECF40B630614DD6A68CD9","masterSlaveType":1},{"id":2,"version":"5.6.1","ip":"127.0.0.1","port":3307,"manageUser":"root","managePassword":"ADB29AB47052A5D0F4D1548AA8CBDCB9","masterSlaveType":2}],"userInfos":[{"id":1,"userName":"test","password":"4761AC79ED0A03FEE7AC24B943BE44E9","host":"127.0.0.1","databases":["test","function_test_1","function_test_2","lzytest2","autotest_3323","utf8mb4testdb"]},{"id":805,"userName":"internal","password":"A9FB8054E867924D3F3AE2C3609247CAA323D9B940F0CEA36572A00F3CD9F6F5","host":"127.0.0.1","databases":["test","function_test_1","function_test_2","lzytest2","autotest_3323","utf8mb4testdb"]}]}]}
 
 分配实例 22   {"taskId":"taskId22","rdsInstances":[{"rdsId":"HotDB_RDS_MM01_1","vip":"192.168.200.214","port":"3323","startAt":"2017-06-30 00:00:01","endAt":"2018-12-30 23:59:59"}]}
 
+
+
+
+
+
+
+
+// ----------------------------------------------------HotDB Cloud Server Cluster 通讯协议完整版--------------------------------------------------------------------------------------
+
+// ---------------------------------------1.2.1 M to RS_RS 编组信息   2--------------------------------------------------
+{
+    "taskId": "uuid",
+    "currentIp": "127.0.0.1",
+    "managerPort": 3323,
+    "managerUser": "root",
+    "managerPassword": "123456",
+    "namesrvAddr": "127.0.0.1:9786",
+    "topic": "topicName",
+    "tags": "tagsName",
+    "systemConfig": {
+        "heartbeatToM": 3000,
+        "usingAIO": 1,
+        "enableHeartbeat": true,
+        "heartbeatPeriod": 3000,
+        "heartbeatNextWaitTimes": 3000,
+        "processors": 16,
+        "processorExecutor": 4,
+        "timerExecutor": 6,
+        "parkPeriod": 100000,
+        "waitForSlaveInFailover": true,
+        
+    },
+    "rdsInstances": [
+        {
+            "rdsId": "HotDB_RDS_MM01_1",
+            "vip": "127.0.0.1",
+            "port": 3306,
+            "modeltype": 1,
+            "maxConnections": 5000,
+            "maxUserConnections": 0,
+            "startAt": "yyyy-MM-dd HH:mm:ss",
+            "endAt": "yyyy-MM-dd HH:mm:ss",
+            "strategyForRWSplit": 0,
+            "readerWeight": "40,30,20,0,10,10",
+            "weightForSlaveRWSplit": 50,
+            "writeStatus": 0,
+            "status": 0,
+            "mysqlInfos": [
+                {
+                    "id": 1,
+                    "version": "5.7",
+                    "ip": "127.0.0.1",
+                    "port": 3306,
+                    "manageUser": "root",
+                    "managePassword": "123456",
+                    "masterSlaveType": 1,
+                    "copyfromId": 1,
+                    "copyfromIp": "127.0.0.1",
+                    "copyfromPort": 3306
+                }
+            ],
+            "dbInfos": [
+                {
+                    "dbId": 1,
+                    "databaseName": "test",
+                    "characterset": "utf8"
+                }
+            ],
+            "userInfos": [
+                {
+                    "id": 1,
+                    "userName": "test",
+                    "password": "test",
+                    "host": "127.0.0.1",
+                    "databases": [
+                        "",
+                        ""
+                    ]
+                }
+            ],
+            "ipWhites": {
+                "isEnable": false,
+                "ips": [
+                    "127.0.0.1",
+                    "%"
+                ]
+            },
+            "wallConfig": [
+                "delete_no_where_disallow",
+                "update_no_where_disallow"
+            ],
+            "sqlAudit": {
+                "rule": [
+                    
+                ],
+                "isEnable": true
+            }
+        }
+    ]
+}
